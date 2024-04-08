488e357526b485f76cbf8813d3106e54bb3c4b1b
Cyphle <cyril.phamle@gmail.com>
2024-04-08 15:18:50 +0200

    notes

--- server.go
+++ server.go
@@ -28,6 +28,7 @@ func main() {
 	fmt.Println("Accounts now: " + db.ToString())
 	fmt.Println("END TEST")
 
+	// TODO Faire des tests avec https://www.codingexplorations.com/blog/testing-gorm-with-sqlmock
 	// GORM
 	dsn := "host=localhost user=postgres password=postgres dbname=banana port=5432 sslmode=disable TimeZone=Europe/Paris"
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
@@ -56,12 +57,16 @@ func main() {
 
 	fmt.Println("Record from database")
 	fmt.Println(product)
+	// END GORM
 
-	// END DATABASE
+	// TODO HTTP Server vanilla Go et check les middleware : https://pkg.go.dev/github.com/nahojer/httprouter#section-readme
 
+	// TODO Faire des tests echo https://echo.labstack.com/docs/testing
+	// ECHO
 	e := echo.New()
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, hello.Hello()+", World!")
 	})
 	e.Logger.Fatal(e.Start(":1323"))
+	// END ECHO
 }
