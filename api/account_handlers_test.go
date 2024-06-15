a0e16a06c044e0bf2db7f1a5cddd7a32aad7c732
Cyphle <cyril.phamle@gmail.com>
2024-06-15 14:26:04 +0200

    test handlers

--- api/account_handlers_test.go
+++ api/account_handlers_test.go
@@ -1,12 +1,31 @@
 package api
 
 import (
+	"banana-back/repositories"
+	"context"
 	"github.com/labstack/echo/v4"
 	"github.com/stretchr/testify/assert"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 )
+
+type MockAccountRepository struct {
+}
+
+func (m MockAccountRepository) List(ctx context.Context) ([]repositories.AccountEntity, error) {
+	res := make([]repositories.AccountEntity, 1)
+	res[0] = repositories.AccountEntity{
+		ID:   1,
+		Name: "Coucou",
+	}
+	return res, nil
+}
+
+func NewHttpHandlerWithMock() HttpHandler[repositories.AccountEntity] {
+	mock := &MockAccountRepository{}
+	return NewHttpHandler(mock)
+}
 
 func TestGetAccounts(t *testing.T) {
 	// Setup
@@ -15,11 +34,11 @@ func TestGetAccounts(t *testing.T) {
 	rec := httptest.NewRecorder()
 	c := e.NewContext(req, rec)
 	c.SetPath("/accounts")
-	h := &HttpHandler{}
+	h := NewHttpHandlerWithMock()
 
 	// Assertions
 	if assert.NoError(t, h.getAccounts(c)) {
 		assert.Equal(t, http.StatusOK, rec.Code)
-		//assert.Equal(t, userJSON, rec.Body.String())
+		assert.Equal(t, "{ data: [{ ID: 1, Name: \"coucou\"}]}", rec.Body.String())
 	}
 }
