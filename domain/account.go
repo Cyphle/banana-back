a583d621e3ff327c0539d72b3ea9f1a66ccf07c6
Cyphle <cyril.phamle@gmail.com>
2024-07-12 17:04:35 +0200

    functional layer

--- domain/account.go
+++ domain/account.go
@@ -14,7 +14,7 @@ type CreateAccountCommand struct {
 	Name string
 }
 
-// TODO to be tested and injected
+// TODO to be tested and injected ou alors transformer en fonctionnel pur sans dépendance
 func CreateAccount(repository Repository[Account], ctx context.Context, request *CreateAccountCommand) (*Account, error) {
 	if existingAccount, err := repository.FindOneByField(ctx, "name", request.Name); err != nil {
 		return nil, fmt.Errorf("failed to create account: %w", err)
@@ -31,4 +31,14 @@ func CreateAccount(repository Repository[Account], ctx context.Context, request 
 	}
 
 	return repository.FindOneByField(ctx, "name", request.Name)
+}
+
+func CreateAccount(command *CreateAccountCommand, existingAccount *Account) (*Account, error) {
+	if existingAccount != nil {
+		return nil, fmt.Errorf("name already exists")
+	}
+
+	return &Account{
+		Name: command.Name,
+	}, nil
 }
