198ca08ff737a33309bf851608b14aa009dc1ce4
Cyphle <cyril.phamle@gmail.com>
2024-11-08 16:53:22 +0100

    session

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 use actix_cors::Cors;
-use actix_session::{SessionMiddleware, storage::CookieSessionStore};
+use actix_session::config::PersistentSession;
 use actix_session::Session;
-use actix_web::cookie::Key;
+use actix_session::{storage::CookieSessionStore, SessionMiddleware};
+use actix_web::cookie::{time, Key};
 use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
 use log::debug;
 use log::error;
@@ -14,9 +15,10 @@ use openid::TokenIntrospection;
 use openid::Userinfo;
 use openid::{Client, Options, StandardClaims, Token};
 use reqwest::Client as HttpClient;
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
 use serde_json::Value;
 use std::collections::HashMap;
+use std::fmt::Display;
 use std::sync::Arc;
 use std::time::Duration;
 use tokio::sync::Mutex;
@@ -76,7 +78,8 @@ async fn main() -> std::io::Result<()> {
     // Wrap client in Arc and Mutex for sharing across Actix handlers
     let client = Arc::new(Mutex::new(client));
     // Generate a secure 32-byte key for cookie signing (use a random key in production)
-    let secret_key = Key::generate();
+    // let secret_key = Key::generate();
+    let secret_key = Key::from(&[0; 64]);
 
     // Start Actix server
     HttpServer::new(move || {
@@ -89,7 +92,15 @@ async fn main() -> std::io::Result<()> {
                     .supports_credentials() // Optional, if credentials are used
                     .max_age(3600),
             )
-            .wrap(SessionMiddleware::new(CookieSessionStore::default(), secret_key.clone()))
+            .wrap(
+                SessionMiddleware::builder(CookieSessionStore::default(), secret_key.clone())
+                    .session_lifecycle(
+                        PersistentSession::default().session_ttl(time::Duration::days(5)),
+                    )
+                    .cookie_secure(false)
+                    .cookie_name("actix_cookie".to_string())
+                    .build(),
+            )
             .app_data(web::Data::new(client.clone()))
             .route("/set_session", web::get().to(set_session))
             .route("/get_session", web::get().to(get_session))
@@ -105,33 +116,84 @@ async fn main() -> std::io::Result<()> {
 }
 
 ////////// SESSION //////////
+
+#[derive(Deserialize, Serialize, Debug, Clone, PartialEq, Eq)]
+pub struct CustomBearer {
+    pub access_token: String,
+    pub token_type: String,
+    pub scope: Option<String>,
+    pub state: Option<String>,
+    pub refresh_token: Option<String>,
+    pub expires_in: Option<u64>,
+    pub id_token: String,
+    #[serde(flatten)]
+    pub extra: Option<HashMap<String, serde_json::Value>>,
+}
+
+impl Display for CustomBearer {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        // match self.id_token.as_ref() {
+        //     Some(id_token) => write!(f, "{} {}", self.access_token, id_token),
+        //     None => write!(f, "{}", self.access_token),
+        // }
+        write!(f, "{} {}", self.access_token, self.id_token)
+    }
+}
+
 // Route to set data in the session
 async fn set_session(session: Session) -> impl Responder {
     let res = session.insert("user_id", 42);
     match res {
-        Ok(_) => HttpResponse::Ok().body("Session data set"),
-        Err(e) => HttpResponse::Ok().body(format!("Error setting session data: {}", e)),
+        Ok(_) => {
+            info!("Session data set");
+            HttpResponse::Ok().body("Session data set")
+        },
+        Err(e) => {
+            error!("Error setting session data: {}", e);
+            HttpResponse::Ok().body(format!("Error setting session data: {}", e))
+        }
+    }
+}
+
+fn save_token_in_session(session: &Session, token: &Bearer) {
+    let saved = session.insert("token", token.clone());
+    match saved {
+        Ok(_) => info!("Token saved in session"),
+        Err(e) => error!("Error saving token in session: {}", e),
     }
 }
 
 // Route to retrieve data from the session
-async fn get_session(session: Session) -> impl Responder {
-    let user_id = session.get::<i32>("user_id");
+async fn get_session(
+    session: Session,
+    client: web::Data<Arc<Mutex<Client<openid::Discovered, StandardClaims>>>>,
+    _: web::Query<AuthRequest>,
+) -> impl Responder {
+    let user_id = session.get::<CustomBearer>("user_id");
 
     match user_id {
-        Ok(user_id) => {
-            match user_id {
-                Some(user_id) => HttpResponse::Ok().body(format!("Welcome back, user {}!", user_id)),
-                None => HttpResponse::Ok().body("No user ID found in session"),
-            }
+        Ok(user_id) => match user_id {
+            Some(user_id) => {
+                info!("User ID found in session: {}", user_id);
+                HttpResponse::Ok().body(format!("Welcome back, user {}!", user_id))
+            },
+            None => {
+                error!("No user ID found in session");
+                HttpResponse::Ok().body("No user ID found in session")
+            },
         },
-        Err(e) => HttpResponse::Ok().body(format!("No session data found: {}", e)),
+        Err(e) => {
+            error!("No session data found: {}", e);
+            HttpResponse::Ok().body(format!("No session data found: {}", e))
+        },
     }
 }
+
 
 ////////// OIDC //////////
 #[get("/login")]
 async fn login(
+    session: Session,
     client: web::Data<Arc<Mutex<Client<openid::Discovered, StandardClaims>>>>,
     query: web::Query<AuthRequest>,
 ) -> impl Responder {
@@ -152,6 +214,29 @@ async fn login(
                 Ok(token) => {
                     let access_token = token.bearer.access_token.clone();
                     let id_token = token.bearer.id_token.clone();
+                    info!("ID token in authenticate: {}", id_token.clone().unwrap());
+
+                    // save_token_in_session(&session, &token.bearer);
+                    // Notes
+                    /*
+                        Bon ça marche pas de serializer tout le token en session. Il faut créer une autre struct qui save les token in memory et avec une clé un hash ou uuid et en session on save l'uuid
+                     */
+                    let bearer: Bearer = token.bearer.clone();
+                    let another_bearer = CustomBearer {
+                        access_token: bearer.access_token.clone(),
+                        token_type: bearer.token_type,
+                        scope: bearer.scope,
+                        state: bearer.state,
+                        refresh_token: bearer.refresh_token,
+                        expires_in: bearer.expires_in,
+                        id_token: id_token.clone().unwrap(),
+                        extra: bearer.extra,
+                    };
+                    let saved = session.insert("user_id", another_bearer);
+                    match saved {
+                        Ok(_) => info!("Token saved in session"),
+                        Err(e) => error!("Error saving token in session: {}", e),
+                    }
 
                     HttpResponse::Ok().json(HashMap::from([
                         ("access_token", access_token),
