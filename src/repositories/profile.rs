b19c47c622fe712962d9f666891939e58f894634
Cyphle <cyril.phamle@gmail.com>
2025-01-18 14:48:30 +0100

    Find one profile by username

--- src/repositories/profile.rs
+++ src/repositories/profile.rs
@@ -1,7 +1,7 @@
 use crate::domain;
 use crate::domain::profile::{CreateProfileCommand, Profile};
 use sea_orm::ActiveValue::Set;
-use sea_orm::{ActiveModelTrait, DatabaseConnection, DbErr, EntityTrait};
+use sea_orm::{ActiveModelTrait, ColumnTrait, DatabaseConnection, DbErr, EntityTrait, QueryFilter};
 use crate::dto::views::profile::ProfileView;
 use entity::profiles::Entity as ProfileEntity;
 
@@ -36,6 +36,20 @@ pub async fn find_one_by_id(db_connexion: &DatabaseConnection, id: i32) -> Resul
         }))
 }
 
+pub async fn find_one_by_username(db_connexion: &DatabaseConnection, username: &str) -> Result<Option<ProfileView>, DbErr> {
+     ProfileEntity::find()
+        .filter(entity::profiles::Column::Username.eq(format!("{}", username).as_str()))
+        .one(db_connexion)
+        .await
+        .map(|m| m.map(|m| ProfileView {
+            id: m.id,
+            username: m.username,
+            email: m.email,
+            first_name: m.first_name,
+            last_name: m.last_name,
+        }))
+}
+
 #[cfg(test)]
 mod tests {
     mod read {
@@ -44,7 +58,7 @@ mod tests {
         use sea_orm::{DatabaseBackend, DbErr, MockDatabase, Transaction};
         use sea_orm::prelude::DateTimeWithTimeZone;
         use crate::dto::views::profile::ProfileView;
-        use crate::repositories::profile::{find_one_by_id};
+        use crate::repositories::profile::{find_one_by_id, find_one_by_username};
 
         #[async_std::test]
         async fn should_find_one_by_id() -> Result<(), DbErr> {
@@ -87,6 +101,55 @@ mod tests {
                         DatabaseBackend::Postgres,
                         r#"SELECT "profiles"."id", "profiles"."username", "profiles"."email", "profiles"."first_name", "profiles"."last_name", "profiles"."created_at", "profiles"."updated_at", "profiles"."deleted_at" FROM "profiles" WHERE "profiles"."id" = $1 LIMIT $2"#,
                         [1.into(), 1u64.into()],
+                    ),
+                ]
+            );
+
+            Ok(())
+        }
+
+        #[async_std::test]
+        async fn should_find_one_by_username() -> Result<(), DbErr> {
+            let d = NaiveDate::from_ymd_opt(2015, 6, 3).unwrap();
+            let t = NaiveTime::from_hms_milli_opt(12, 34, 56, 789).unwrap();
+
+            let db = MockDatabase::new(DatabaseBackend::Postgres)
+                .append_query_results([
+                    vec![entity::profiles::Model {
+                        id: 1,
+                        username: "johndoe".to_owned(),
+                        email: "johndoe@banana.fr".to_owned(),
+                        first_name: "John".to_owned(),
+                        last_name: "Doe".to_owned(),
+                        created_at: DateTimeWithTimeZone::from_naive_utc_and_offset(NaiveDateTime::new(d, t), FixedOffset::east_opt(0).unwrap()),
+                        updated_at: DateTimeWithTimeZone::from_naive_utc_and_offset(NaiveDateTime::new(d, t), FixedOffset::east_opt(0).unwrap()),
+                        deleted_at: None,
+                    }],
+                ])
+                .into_connection();
+
+            let found = find_one_by_username(&db, "johndoe").await?;
+
+
+            assert_eq!(
+                found,
+                Some(ProfileView {
+                    id: 1,
+                    username: "johndoe".to_owned(),
+                    email: "johndoe@banana.fr".to_owned(),
+                    first_name: "John".to_owned(),
+                    last_name: "Doe".to_owned(),
+                })
+            );
+
+            // Checking transaction log
+            assert_eq!(
+                db.into_transaction_log(),
+                [
+                    Transaction::from_sql_and_values(
+                        DatabaseBackend::Postgres,
+                        r#"SELECT "profiles"."id", "profiles"."username", "profiles"."email", "profiles"."first_name", "profiles"."last_name", "profiles"."created_at", "profiles"."updated_at", "profiles"."deleted_at" FROM "profiles" WHERE "profiles"."username" = $1 LIMIT $2"#,
+                        ["johndoe".into(), 1u64.into()],
                     ),
                 ]
             );
