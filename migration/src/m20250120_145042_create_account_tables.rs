8b667e658a654329ed2d2b174e956451be0bab55
Cyphle <cyril.phamle@gmail.com>
2025-01-20 16:16:28 +0100

    accounts migration

---
+++ migration/src/m20250120_145042_create_account_tables.rs
@@ -0,0 +1,47 @@
+use sea_orm_migration::{prelude::*, schema::*};
+
+#[derive(DeriveMigrationName)]
+pub struct Migration;
+
+#[async_trait::async_trait]
+impl MigrationTrait for Migration {
+    async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        let db = manager.get_connection();
+
+        // type: PERSONAL, PERSONAL_SHARED, PERSONAL_SAVINGS, SHARED_SAVINGS
+        db.execute_unprepared(
+            "CREATE TABLE IF NOT EXISTS accounts(
+                id              SERIAL,
+                name            VARCHAR(255) NOT NULL,
+                type            VARCHAR(255) NOT NULL,
+                user_id         INTEGER NOT NULL,
+                starting_amount NUMERIC(20, 2)
+                created_at      timestamptz NOT NULL DEFAULT now(),
+                updated_at      timestamptz NOT NULL DEFAULT now(),
+                deleted_at      timestamptz,
+                PRIMARY KEY (id),
+                CONSTRAINT fk_accounts_users FOREIGN KEY (user_id) REFERENCES users (id)
+                ON DELETE CASCADE
+                ON UPDATE CASCADE
+            )"
+        )
+            .await?;
+
+        db.execute_unprepared(
+            ""
+        )
+            .await?;
+
+        Ok(())
+    }
+
+    async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        manager
+            .get_connection()
+            .execute_unprepared("DROP TABLE IF EXISTS accounts;")
+            .await?;
+
+        Ok(())
+    }
+}
+
